2c6f557e2417d8cea882e9c406577de8397e7dc6
mhawkins <megan.hawkins28@gmail.com>
2025-11-10 15:53:11 +0100

    work on landing page

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { Routes, Route } from "react-router";
+
+import NavBar from "./components/NavBar/NavBar";
+import SignUpForm from "./components/SignUpForm/SignUpForm";
+import SignInForm from "./components/SignInForm/SignInForm";
+
+const App = () => {
+  return (
+    <>
+      <NavBar />
+
+      
+      <Routes>
+        <Route path="/sign-up" element={<SignUpForm />} />
+        <Route path="/sign-in" element={<SignInForm />} />
+      </Routes>
+    </>
+  );
+};
+
+export default App;
